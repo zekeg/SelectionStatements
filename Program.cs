792caf9dfea40b4a6ab3451d1d7c12d19d35317e
Zechariah Guadiana <zechariah.guadiana@gmail.com>
2022-03-08 02:11:31 -0600

    Initial commit for selection statements demonstration for true coders

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SelectionStatements;
+
+internal class SelectionStatements
+{
+    static void Main()
+    {
+        var r = new Random();
+        int favNum = r.Next(1, 10);
+        Console.WriteLine("Please guess a number between 1 and 3");
+        int guessedNum = int.Parse(Console.ReadLine());
+
+        
+        if (guessedNum > favNum)
+        {
+            Console.WriteLine("Too high");
+        }
+        else if (guessedNum < favNum)
+        {
+            Console.WriteLine("Too low");
+        }
+        else
+        {
+            Console.WriteLine("Nevermind");
+        }
+    }
+}   
